2a85937b02d768c5e05855d2e45b9b250cbb2cd3
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-07-11 10:50:31 +0200

    I'll explain when you're older!

--- build/build.zig
+++ build/build.zig
@@ -1,11 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const windows = b.option(bool, "windows", "Target MS Windows") orelse false;
+
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_source_file = b.path("hello.zig"),
-        .target = b.graph.host,
+        .target = b.resolveTargetQuery(.{
+            .os_tag = if (windows) .windows else null,
+        }),
     });
 
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the hello program");
+    run_step.dependOn(&run_exe.step);
 }
