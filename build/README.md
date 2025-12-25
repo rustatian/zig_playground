5384aa4b3b07efb218c262c1a941dd89b0e9ec72
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-12-25 19:11:57 +0100

    chore: make it work
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ build/README.md
@@ -0,0 +1,2 @@
+1. zig build -Dversion=1.2.3 --summary all - no error
+2. zig build -- would be an error
--- build/build.zig
+++ build/build.zig
@@ -1,10 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
         .name = "app",
-        .root_source_file = b.path("app.zig"),
-        .target = b.graph.host,
+        .root_module = b.createModule(.{
+            // b.createModule defines a new module just like b.addModule but,
+            // unlike b.addModule, it does not expose the module to consumers of
+            // this package, which is why in this case we don't have to give it a name.
+            .root_source_file = b.path("src/app.zig"),
+            // Target and optimization levels must be explicitly wired in when
+            // defining an executable or library (in the root module), and you
+            // can also hardcode a specific target for an executable or library
+            // definition if desireable (e.g. firmware for embedded devices).
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const version = b.option([]const u8, "version", "application version string") orelse "0.0.0";
